9c44816474b6f6f8198adbc2353191e1e5d99224
Andrew Mackenzie <andrew@mackenzie-serres.net>
2018-08-12 23:11:38 +0200

    Fix Go execution

--- go/run.sh
+++ go/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 datasets=("heathrow-london")
+cd src
 for d in ${datasets[*]} 
 do
     echo "Running for road system '"$d"'"
-    time ./router ../data/$d.in
+    timE ./router ../../data/$d.in
 done
+cd -
